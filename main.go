d2221452267bc927aff292f0a26d4ca56a036687
Tricky1975 <facebook@tbbs.nl>
2018-04-28 23:37:59 +0200

    Menu

--- main.go
+++ main.go
@@ -1,8 +1,53 @@
 package main
 
 import(
-_	"github.com/TrickyGameJolt/GoGameJolt"
+	"github.com/TrickyGameJolt/GoGameJolt" // Please note, this comes in as package gj and not as package GoGameJolt
+	"bufio"
+	"strings"
+	"fmt"
+	"os"
 )
 
+const gameid="336383"
+const gamekey="e8a4b4be97e11da42183a5751cef877b"
+var username=""
+var token=""
+
+
+func MyTrim(a string) string{
+	return strings.Trim(a," \t\n\r\x00")
+}
+
+func RawInput(q string) string{
+    buf := bufio.NewReader(os.Stdin)
+    fmt.Print(q)
+    sentence, err := buf.ReadBytes('\n')
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    } else {
+		//fmt.Println("*"+string(sentence)+"*")
+        return MyTrim(string(sentence))
+    }
+}
+
 func main(){
+	// Authentication Test
+	username=RawInput("User Name: ")
+	token   =RawInput("    Token: ")
+	user:=gj.Login(gameid,gamekey,username,token)
+	//fmt.Println("Logged in = ",user.LoggedIn)
+	if !user.LoggedIn { os.Exit(0) }
+	for{
+		fmt.Println("1 = Test session")
+		fmt.Println("2 = Test achievement/trophy")
+		fmt.Println("3 = Test score")
+		fmt.Println("Q = Quit")
+		kz:=RawInput("Make your choice: ")
+		if kz=="q" || kz=="Q" { break }
+		switch kz{			
+			default: 
+				fmt.Println("I don't understand. Please try again!")
+		}
+	}
 }
